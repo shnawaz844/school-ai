40833d47c2cc8cff708cddabbe83453885209889
shnawaz844 <shnawaz844@users.noreply.github.com>
2026-04-03 01:59:38 +0530

    language done short

--- shared/list.tsx
+++ shared/list.tsx
@@ -6,7 +6,7 @@ export const AITeacherAgents = [
         description: "Guides students through essential physics concepts.",
         image: "/physics1.jpg",
         agentPrompt:
-            "Hello! I am your Physics educator. Aaj hum mechanics, electromagnetism, aur optics jaise core topics par focus karenge. Mera approach interactive rahega—mai aapse derivations aur numerical-based questions poochunga jo competitive exams ke liye zaroori hain. Agar calculations ya logic mein doubt ho, toh be-jhijhak poochiye. Physics ko logic se samjhenge. Shall we begin?",
+           " Hi! 🔥 Physics made simple & powerful—let’s dive in ! What do you want to learn today?",
         voiceId: "Arjun",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -18,7 +18,7 @@ export const AITeacherAgents = [
         description: "Guides students through essential biology concepts.",
         image: "/biology1.jpg",
         agentPrompt:
-            "Hello! I am your Biology educator. Aaj hum life sciences ke complex processes, diagrams, aur functions ko bahut saral bhasha mein samjhenge. Mai aapse terminologies aur theory-based questions poochungi, jaise board exams aur NEET mein aate hain. Agar aapko koi diagram ya cycle samajhne mein mushkil ho, mai yahan hoon help karne ke liye. Chaliye, biological world ko explore karte hain!",
+            "Hello! I am your Biology educator. Chaliye, biological world ko explore karte hain! To aaj aap kya sekhna chahte hai",
         voiceId: "eileen",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -30,7 +30,7 @@ export const AITeacherAgents = [
         description: "Improves grammar, writing, and literature skills.",
         image: "/english.jpg",
         agentPrompt:
-            "Greetings! I am your English language coach. Our goal is to master Grammar, Writing skills, and Literature. I will ask you questions about sentence structures, vocabulary, and chapter analysis to improve your fluency and confidence. I will explain rules in a mix of Hindi and English so you can grasp them perfectly. Ready to polish your English skills? Let’s proceed.",
+            "I am your English language educator. Let’s Begin. What do you want to learn today?",
         voiceId: "charlotte",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -42,7 +42,7 @@ export const AITeacherAgents = [
         description: "Covers Hindi grammar, literature, and writing.",
         image: "/hindi.jpg",
         agentPrompt:
-            "Namaste! Mai aapki Hindi shikshika hoon. Hum Hindi vyakaran, sahitya (literature), aur creative writing par dhayan denge. Mai aapse kavitaon ke bhav aur vyakaran ke niyam poochungi, jo aapki bhasha aur exam performance dono ko behtar banayenge. Agar kisi shabd ka arth ya vyakhya samajh na aaye, toh mai usey saral tarike se samjhaungi. Chaliye, apni matrubhasha ko aur acche se samajhte hain.",
+            "Namaste! Mai aapki Hindi shikshika hoon. Chaliye, apni matrbhasha ko aur acche se samajhte hain.To aaj aap kya sekhna chahte hai?",
         voiceId: "ayla",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -53,7 +53,7 @@ export const AITeacherAgents = [
         description: "Covers Physical, Organic, and Inorganic Chemistry.",
         image: "/chemistry2.jpg",
         agentPrompt:
-            "Hi there! Mai aapka Chemistry expert hoon. Hum Physical calculations, Organic reactions, aur Inorganic concepts ko cover karenge. Mai aapse reaction mechanisms aur periodic trends se related questions poochunga. Mera goal hai ki aap Chemistry ko ratne ke bajaye uske piche ka logic samjhein. Mai aapko Bilingual (Hindi-English) mein support karunga. Let's start the reaction!",
+            "Hi there! Mai aapka Chemistry expert hoon.To aaj aap Chemistry me kya sekhna chahte hai?",
         voiceId: "susan",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -65,7 +65,7 @@ export const AITeacherAgents = [
         description: "Teaches History, Geography, Civics, and Economics.",
         image: "/socialscience3.jpg",
         agentPrompt:
-            "Hi! Mai aapki Social Science guide hoon. Hum History ke events, Geography ke maps, Civics ke rules, aur Economics ke concepts ko discuss karenge. Mai aapse short case studies aur factual questions poochungi taaki aap topics ko ratne ke bajaye connect kar sakein. Sab kuch bahut simple Hindi aur English mein hoga. Kya aap history aur geography ki journey par chalne ke liye taiyar hain?",
+            "Hi! Mai aapki Social Science guide hoon. Kya aap history aur geography ki journey par chalne ke liye taiyar hain? To batae aaj aap kya sekhna chahte hai?",
         voiceId: "aaliyah",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -77,7 +77,7 @@ export const AITeacherAgents = [
         description: "Teaches coding, logic, and computer fundamentals.",
         image: "/computer.jpg",
         agentPrompt:
-            "Hello World! I am your Computer Science mentor. Aaj hum coding logic, algorithms, aur computer fundamentals par kaam karenge. Mai aapse logical programming questions aur theory-based questions poochunga. Coding ek skill hai, aur mai aapko syntax se lekar logic building tak sab kuch simple Hindi/English mein sikhaunga. Don't worry about bugs, we will solve them together. Ready to code?",
+            "Hello ! I am your Computer Science mentor.Are you Ready to code? To aaj aap Computer Science me kya sekhna chahte hai?",
         voiceId: "hudson",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -89,7 +89,7 @@ export const AITeacherAgents = [
         description: "Covers Accountancy, Business Studies, and Economics.",
         image: "/commerce.jpg",
         agentPrompt:
-            "Hi! Mai aapka Commerce specialist hoon. Hum Accountancy ki balance sheets, Business Studies ke management principles, aur Economics ke market trends par focus karenge. Mai aapse numerical aur conceptual questions poochunga jo bilkul practical aur exam-oriented honge. Business logic ho ya accounting debit-credit, mai aapko clear Hindi/English mein guide karunga. Let’s build your professional foundation!",
+            "Hi! Mai aapka Commerce specialist hoon. Let’s build your professional foundation! To aaj aap Commerce me kya sekhna chahte hai?",
         voiceId: "atlas",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -100,7 +100,7 @@ export const AITeacherAgents = [
         description: "Helps students understand maths concepts from basics to advanced.",
         image: "/math.jpg",
         agentPrompt:
-            "Hi! I am your Mathematics mentor. Aaj hum math ke concepts ko simplify karenge. Mai aapse step-by-step questions poochungi—basic formulas se lekar complex problems tak—bilkul ek question-answer session ki tarah. Agar aap kahin bhi atak jayein, toh don't worry, mai aapko simple Hindi aur English mein guide karungi. Let's make math easy. Kya aap shuru karne ke liye taiyar hain?",
+            "Hi! I am your Mathematics mentor. Let's make math easy. Kya aap shuru karne ke liye taiyar hain? To aaj aap Mathematics me kya sekhna chahte hai?",
         voiceId: "nikita",
         gender: "female" as const,
         subscriptionRequired: false,
@@ -111,7 +111,7 @@ export const AITeacherAgents = [
         description: "Covers Physics, Chemistry, and Biology.",
         image: "/science1.jpg",
         agentPrompt:
-            "Hello! I am your Science teacher. Aaj hum Physics, Chemistry, aur Biology ke interesting topics explore karenge, jaise Cell Structure, States of Matter, ya Light Reflection. Mai aapse conceptual aur application-based questions poochunga jo aapke school aur board exams ke liye bahut important hain. Mai har concept ko simple language mein explain karunga taaki aapka foundation strong ho sake. Let's dive into the world of science!",
+            "Hello! I am your Science teacher. Let's dive into the world of science! So tell me what do you want to learn today?",
         voiceId: "chris",
         gender: "male" as const,
         subscriptionRequired: true,
