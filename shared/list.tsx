d14d05acbd3b3bff5b06d93225ee962d88976b97
shnawaz844 <shnawaz844@users.noreply.github.com>
2026-04-03 12:26:39 +0530

    ne changes

--- shared/list.tsx
+++ shared/list.tsx
@@ -6,7 +6,7 @@ export const AITeacherAgents = [
         description: "Guides students through essential physics concepts.",
         image: "/physics1.jpg",
         agentPrompt:
-           " Hi! 🔥 Physics made simple & powerful—let’s dive in ! What do you want to learn today?",
+            " Hi! 🔥 Mai aapka Physics expert hoon.To aaj aap Physics me kya seekhna chahte hai?",
         voiceId: "Arjun",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -18,7 +18,7 @@ export const AITeacherAgents = [
         description: "Guides students through essential biology concepts.",
         image: "/biology1.jpg",
         agentPrompt:
-            "Hello! I am your Biology educator. Chaliye, biological world ko explore karte hain! To aaj aap kya sekhna chahte hai",
+            "Hello! Mai aapka Biology expert hoon.To aaj aap Biology me kya seekhna chahte hai?",
         voiceId: "eileen",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -30,7 +30,7 @@ export const AITeacherAgents = [
         description: "Improves grammar, writing, and literature skills.",
         image: "/english.jpg",
         agentPrompt:
-            "I am your English language educator. Let’s Begin. What do you want to learn today?",
+            "Hello! Mai aapka English expert hoon.To aaj aap English me kya seekhna chahte hai?",
         voiceId: "charlotte",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -42,7 +42,7 @@ export const AITeacherAgents = [
         description: "Covers Hindi grammar, literature, and writing.",
         image: "/hindi.jpg",
         agentPrompt:
-            "Namaste! Mai aapki Hindi shikshika hoon. Chaliye, apni matrbhasha ko aur acche se samajhte hain.To aaj aap kya sekhna chahte hai?",
+            "Namaste! Mai aapki Hindi shikshika hoon. Chaliye, apni maatrbhasha ko aur acche se samajhte hain.To aaj aap kya seekhna chahte hai?",
         voiceId: "ayla",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -53,7 +53,7 @@ export const AITeacherAgents = [
         description: "Covers Physical, Organic, and Inorganic Chemistry.",
         image: "/chemistry2.jpg",
         agentPrompt:
-            "Hi there! Mai aapka Chemistry expert hoon.To aaj aap Chemistry me kya sekhna chahte hai?",
+            "Hi ! Mai aapka Chemistry expert hoon.To aaj aap Chemistry me kya seekhna chahte hai?",
         voiceId: "susan",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -65,7 +65,7 @@ export const AITeacherAgents = [
         description: "Teaches History, Geography, Civics, and Economics.",
         image: "/socialscience3.jpg",
         agentPrompt:
-            "Hi! Mai aapki Social Science guide hoon. Kya aap history aur geography ki journey par chalne ke liye taiyar hain? To batae aaj aap kya sekhna chahte hai?",
+            "Hi! Mai aapki Social Science guide hoon. Kya aap history aur geography ki journey par chalne ke liye taiyar hain? To batae aaj aap kya seekhna chahte hai?",
         voiceId: "aaliyah",
         gender: "female" as const,
         subscriptionRequired: true,
@@ -77,7 +77,7 @@ export const AITeacherAgents = [
         description: "Teaches coding, logic, and computer fundamentals.",
         image: "/computer.jpg",
         agentPrompt:
-            "Hello ! I am your Computer Science mentor.Are you Ready to code? To aaj aap Computer Science me kya sekhna chahte hai?",
+            "Hello ! Mai aapka Computer Science mentor hoon.Are you Ready to code? To aaj aap Computer Science me kya seekhna chahte hai?",
         voiceId: "hudson",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -89,7 +89,7 @@ export const AITeacherAgents = [
         description: "Covers Accountancy, Business Studies, and Economics.",
         image: "/commerce.jpg",
         agentPrompt:
-            "Hi! Mai aapka Commerce specialist hoon. Let’s build your professional foundation! To aaj aap Commerce me kya sekhna chahte hai?",
+            "Hi! Mai aapka Commerce specialist hoon. Let’s build your professional foundation! To aaj aap Commerce me kya seekhna chahte hai?",
         voiceId: "atlas",
         gender: "male" as const,
         subscriptionRequired: true,
@@ -100,7 +100,7 @@ export const AITeacherAgents = [
         description: "Helps students understand maths concepts from basics to advanced.",
         image: "/math.jpg",
         agentPrompt:
-            "Hi! I am your Mathematics mentor. Let's make math easy. Kya aap shuru karne ke liye taiyar hain? To aaj aap Mathematics me kya sekhna chahte hai?",
+            "Hi! Mai aapki Mathematics mentor hoon. Mai aapki Mathematics ko easy bana dungi. Kya aap shuru karne ke liye taiyar hain? To aaj aap Mathematics me kya seekhna chahte hai?",
         voiceId: "nikita",
         gender: "female" as const,
         subscriptionRequired: false,
@@ -111,7 +111,7 @@ export const AITeacherAgents = [
         description: "Covers Physics, Chemistry, and Biology.",
         image: "/science1.jpg",
         agentPrompt:
-            "Hello! I am your Science teacher. Let's dive into the world of science! So tell me what do you want to learn today?",
+            "Hello! Mai aapka Science teacher hoon. So tell me aaj aap kya seekhna chahte hai?",
         voiceId: "chris",
         gender: "male" as const,
         subscriptionRequired: true,
